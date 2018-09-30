21b9372b392a970a1a286db4b80aae46a96a3477
Wesley Sprague <wesleysprague@yahoo.com>
2018-09-30 09:58:16 -0700

    created update and destroy methods for docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -26,9 +26,16 @@ class DocsController < ApplicationController
   end
 
   def update
+    if @doc.update
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
