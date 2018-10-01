c8ff82bdc50bbac238b4ebd36e9a5afa26f6bf5c
Wesley Sprague <wesleysprague@yahoo.com>
2018-10-01 14:18:18 -0700

    changed routes for logged in user to go to docs index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root "docs#index", as: "logged_in_root"
+  end
   root to: 'pages#home'
   resources :docs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
